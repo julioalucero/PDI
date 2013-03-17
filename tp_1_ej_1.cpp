b750250129ce0a8193423fc1dffc363dad8d3a9f
Julio Lucero <poramo@gmail.com>
2013-03-17 19:26:11 -0300

    1.1 finish with one error

--- tp_1_ej_1.cpp
+++ tp_1_ej_1.cpp
@@ -1,5 +1,9 @@
 #include <CImg.h>
+#include <iostream>
+#include <stdio.h>
+
 using namespace cimg_library;
+using namespace std;
 
 int main(int argc, char *argv[]) {
 
@@ -11,6 +15,13 @@ int main(int argc, char *argv[]) {
   CImg<unsigned char> sub_image(cameramm_image);
   cameramm_image.crop(100, 50, 150, 100);
 
+  // calculate sub_image size (54 of head)
+  // TODO something is wrong on the size calculation
+  float size = 0;
+  size = 50 * 50 * 3 + 54;
+  cout << "Size of the image: " << size << " bytes" << endl;
+
+  sub_image.save("public/images/sub_cameraman.bmp");
   // see two images at the same time
   CImgDisplay ventana(cameramm_image, "cameramm"), 
     sub_ventana(sub_image, "cameramm");
