53ea4689f7282d2a2c01b0100411215b9339f4a7
Julio Lucero <poramo@gmail.com>
2013-03-20 17:50:25 -0300

    exercise 1.4 finish

--- tp_1_ej_4.cpp
+++ tp_1_ej_4.cpp
@@ -5,8 +5,16 @@
 using namespace cimg_library;
 using namespace std;
 
-int main(int argc, char *argv[]) {
+CImg <unsigned char> centered_circle(int image_size, int background_color, int radio, unsigned char circle_color[]){
 
+  CImg <unsigned char> centered_circle_img(image_size, image_size, 1, 3, background_color);
+  centered_circle_img.draw_circle(image_size/2, image_size/2, radio, circle_color, 1);
+
+  return centered_circle_img;
+}
+
+
+int main(int argc, char *argv[]) {
   // Random Gray Columns
   CImg <unsigned char> random_gray_columns(256, 256);
 
@@ -42,9 +50,24 @@ int main(int argc, char *argv[]) {
               ventana_2(random_gray_column_less_100, "Random Gray Columns less 100"),
               ventana_3(black_white, "Black and white in each pixel");
 
+  // Black Image with a white centered circle
+    int image_size;
+    int radio;
+    int background_color;
 
+    cout << "Size of the image: ";
+    cin >> image_size;
+    cout << "background_color: ";
+    cin >> background_color;
+    cout << "radio: ";
+    cin >> radio;
 
+    unsigned char circle_color[]     = {255, 255, 255};    
 
-  while (!ventana_1.is_closed() && !ventana_1.is_keyQ()) {}
+    CImg <unsigned char> centered_circle_img;
+    centered_circle_img = centered_circle(image_size, background_color, radio, circle_color);
+
+  CImgDisplay centered_circle_windows(centered_circle_img, "Centered Circle on Image");
+  while (!centered_circle_windows.is_closed()) {}
   return 0;
 }
