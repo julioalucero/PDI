3f7065249e21f8d58ed8924a178edcc3bb65fe63
Julio Lucero <poramo@gmail.com>
2013-03-24 18:00:21 -0300

    tp 2 ej 2 with errors

---
+++ tp2/tp_2_ej_2.cpp
@@ -0,0 +1,53 @@
+#include <CImg.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace cimg_library;
+using namespace std;
+
+// s = c * log(1+r)
+CImg<unsigned char> cimg_log(CImg<unsigned char> r, float c){
+  CImg<unsigned char> r_normalize = r.get_normalize(0,1);
+  CImg<unsigned char> s(r.width(), r.height(), r.depth(), r.spectrum());
+
+  cimg_forXYC(r_normalize, x, y, v){
+    s(x, y) = c * log( 1 + r_normalize(x,y)); 
+    if ( s(x, y, 0, v) > 1 )
+       s(x, y, 0, v) = 1; 
+    else if ( s(x, y, 0, v) < 0 )
+       s(x, y, 0, v) = 0; 
+  }
+  return s;
+} 
+
+// s = c * r^&
+CImg<unsigned char> cimg_power(CImg<unsigned char> r, float c, float gamma){
+  CImg<unsigned char> r_normalize = r.get_normalize(0,1);
+  CImg<unsigned char> s(r.width(), r.height(), r.depth(), r.spectrum());
+
+  cimg_forXYC(r_normalize, x, y, v){
+    s(x, y, 0, v) = c * pow(r_normalize(x, y, 0, v), gamma); 
+    if ( s(x, y, 0, v) > 1 )
+       s(x, y, 0, v) = 1; 
+    else if ( s(x, y, 0, v) < 0 )
+       s(x, y, 0, v) = 0; 
+  }
+
+  return s;
+} 
+
+int main(int argc, char *argv[]) {
+  CImg<unsigned char> img("../public/images/log.png");
+
+   float gamma = 1;
+   CImg<unsigned char> log = cimg_log(img, 2);
+   CImg<unsigned char> power = cimg_power(img, 1, gamma);
+
+  CImgDisplay ventana(img, "Image Original");
+  CImgDisplay ventana_2(log, "log");
+  CImgDisplay ventana_3(power, "power");
+
+  while (!ventana.is_closed() && !ventana.is_keyQ()) {}
+
+  return 0;
+}
