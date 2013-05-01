d8814bf206873db349ee28a023f22c3d2d7c4cd4
Julio Lucero <poramo@gmail.com>
2013-05-01 16:38:50 -0300

    butterworth filter

---
+++ tp5/tp_5_ej_4_2.cpp
@@ -0,0 +1,64 @@
+#include <CImg.h>
+#include <iostream>
+#include <stdio.h>
+#include <complex>
+
+using namespace cimg_library;
+using namespace std;
+
+#define cimg_use_fftw3
+
+// D0 = Cut Frecuency
+CImgList<double> butterworth(int width, int height, int D0, int order){
+  CImgList<double> filter(2, width, height, 1, 1, 0);
+
+  int center_x = width / 2;
+  int center_y = height / 2;
+
+  cimg_forXY(filter[0], x, y){
+    double factor = sqrt((x-center_x)*(x-center_x) + (y-center_y)*(y - center_y));
+    filter[0](x,y) = 1 / ( 1 + pow(factor/float(D0),  2*order));
+  }
+
+  filter[0].shift(center_x, center_y, 0, 0, 2);
+
+  return filter;
+}
+
+CImg<double> frequency_filter(CImg<double> img, CImgList<double> filter){
+  CImgList<double> fft_img = img.get_FFT();
+  CImgList<double> result(2, img.width(), img.height(), img.depth(), img.spectrum(), 0);
+
+  cimg_forXY(img, u, v){
+    complex <double>
+      complex_img_fft(fft_img[0](u,v), fft_img[1](u,v)),
+      complex_filter(  filter[0](u,v),  filter[1](u,v));
+  
+    // producto de binomios
+    complex<double> complex_product = complex_img_fft * complex_filter;
+
+    // real and imaginary
+    result[0](u,v) = real(complex_product);
+    result[1](u,v) = imag(complex_product);
+  }
+
+  return result.get_FFT(true)[0];
+}
+
+int main(int argc, char *argv[]) {
+
+  // Exercise 5.4.2
+  CImg<double> img("../public/images/huang2.jpg");
+  int D0 = 20; // D0 = Cut Frecuency
+  int order = 4;
+
+  CImgList<double> filter = butterworth(img.width(), img.height(), D0, order);
+
+  CImg<double> img_filter = frequency_filter(img, filter);
+
+  CImgDisplay vent_1(img, "Image");
+  CImgDisplay vent_2(img_filter, "Image with Butterworth Filter");
+
+  while(!vent_1.is_closed()){}; 
+  return 0;
+}
